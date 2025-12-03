065fe8ec1118b37a311e1a2ab8bea96a0e0b9e36
anujareddyy <anujareddy804@gmail.com>
2025-12-03 13:49:08 +0000

    Updated day6 terraform files

--- day6/main.tf
+++ day6/main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-}
-
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
-}
-
-
 resource "aws_instance" "example1" {
     ami = "ami-0fa3fe0fa7920f68e" # Amazon Linux 2 AMI (HVM), SSD Volume Type
     instance_type = var.instance_type
